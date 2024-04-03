7f880fcefa589578e5f1b1326f907727806a0bb9
frozzka <egor.k.19981611@inbox.ru>
2024-04-03 22:15:07 +0500

    Создание абстрактного класса Animal

--- App/Animal.php
+++ App/Animal.php
@@ -1,6 +1,26 @@
 <?php
 
+/**
+ * Родительский класс животных
+ */
 abstract class Animal
 {
+    /**
+     * @var string $id Уникальный ID животного
+     */
+    public string $animalId;
 
+    /**
+     * Метод присваивает уникальный ID животному
+     */
+    public function __construct()
+    {
+        $this->animalId = uniqid(php_uname('n'), true);
+    }
+
+    /**
+     * Метод должен возвращать случайное число продуктов в заданном диапазоне
+     * @return int
+     */
+    abstract public function getProducts():int;
 }
